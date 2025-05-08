53e829427289f620f3a61c7a99f74518e18dfde2
Gabriel Rodríguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2025-05-08 17:49:53 -0300

    Format Scaffold in main.dart for improved readability and structure

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple,
+        body: Center(child: Text('Hello World!')),
+      ),
     ),
   );
 }
