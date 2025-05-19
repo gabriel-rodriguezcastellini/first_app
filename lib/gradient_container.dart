92edf21949ccba438c869af9136d8de89151a0a7
Gabriel Rodríguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2025-05-19 12:34:23 -0300

    Update StyledText to accept a text parameter for dynamic content rendering

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -20,7 +20,7 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(child: StyledText()),
+      child: const Center(child: StyledText('Hello World!')),
     );
   }
 }
