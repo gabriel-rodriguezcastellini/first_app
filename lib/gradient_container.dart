0bb86b6ad594399b6598d04ad3e609197224eaa4
Gabriel Rodríguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2025-05-25 14:50:43 -0300

    Add rollDice method and update GradientContainer layout to include a button

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -9,6 +9,10 @@ class GradientContainer extends StatelessWidget {
   final Color color1;
   final Color color2;
 
+  void rollDice() {
+    // ...
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -19,7 +23,14 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(child: Image.asset('assets/images/dice-2.png', width: 200)),
+      child: Center(
+        child: Column(
+          children: [
+            Image.asset('assets/images/dice-2.png', width: 200),
+            TextButton(onPressed: rollDice, child: const Text('Roll Dice')),
+          ],
+        ),
+      ),
     );
   }
 }
