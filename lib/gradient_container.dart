35de12b55134c5c19e1edcef652a12b80bbd4699
Gabriel Rodríguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2025-06-01 13:40:41 -0300

    Refactor DiceRoller and GradientContainer to improve structure and styling; update main.dart for const usage

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,20 +1,14 @@
+import 'package:first_app/dice_roller.dart';
 import 'package:flutter/material.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  GradientContainer(this.color1, this.color2, {super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
 
   final Color color1;
   final Color color2;
-
-  var activeDiceImage = 'assets/images/dice-2.png';
-
-  void rollDice() {
-    activeDiceImage = 'assets/images/dice-4.png';
-    print('Changing image...');
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,23 +20,7 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(activeDiceImage, width: 200),
-            const SizedBox(height: 20),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(fontSize: 28),
-              ),
-              child: const Text('Roll Dice'),
-            ),
-          ],
-        ),
-      ),
+      child: const Center(child: DiceRoller()),
     );
   }
 }
