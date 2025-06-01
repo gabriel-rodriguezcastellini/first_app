17081b17805ca72706060705b4b559a0087ee2a1
Gabriel Rodríguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2025-05-31 21:54:16 -0300

    Implement initial structure for DiceRoller widget

---
+++ lib/dice_roller.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DiceRoller extends StatefulWidget {  
+  @override
+  State<DiceRoller> createState() {
+    return 
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller>{
+  
+}
